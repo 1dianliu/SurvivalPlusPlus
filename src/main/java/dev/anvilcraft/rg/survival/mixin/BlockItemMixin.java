a3147e42fad0fc2b2bed82e47d4b40dd2b2f79e0
Gugle <administration@ztxy666.cn>
2025-04-25 14:09:52 +0800

    修改 BlockItemMixin

--- src/main/java/dev/anvilcraft/rg/survival/mixin/BlockItemMixin.java
+++ src/main/java/dev/anvilcraft/rg/survival/mixin/BlockItemMixin.java
@@ -1,37 +1,22 @@
 package dev.anvilcraft.rg.survival.mixin;
 
-import dev.anvilcraft.rg.survival.SurvivalPlusPlusServerRules;
-import dev.anvilcraft.rg.survival.event.CanSpectatingPlaceEvent;
-import net.minecraft.core.BlockPos;
-import net.minecraft.world.entity.player.Player;
+import dev.anvilcraft.rg.survival.event.PlayerCanPlaceEvent;
 import net.minecraft.world.item.BlockItem;
 import net.minecraft.world.item.context.BlockPlaceContext;
-import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.state.BlockState;
-import net.minecraft.world.phys.shapes.CollisionContext;
-import net.minecraft.world.phys.shapes.VoxelShape;
 import net.neoforged.neoforge.common.NeoForge;
+import org.jetbrains.annotations.NotNull;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Redirect;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(BlockItem.class)
 abstract class BlockItemMixin {
-    @Redirect(method = "canPlace", at = @At(
-        value = "INVOKE",
-        target = "Lnet/minecraft/world/level/Level;isUnobstructed(Lnet/minecraft/world/level/block/state/BlockState;Lnet/minecraft/core/BlockPos;Lnet/minecraft/world/phys/shapes/CollisionContext;)Z"
-    ))
-    private boolean canSpectatingPlace(Level world, BlockState state, BlockPos pos, CollisionContext context,
-                                       BlockPlaceContext contextOuter, BlockState stateOuter) {
-        Player player = contextOuter.getPlayer();
-        if (SurvivalPlusPlusServerRules.creativeNoClip && player != null && player.isCreative() && player.getAbilities().flying) {
-            // copy from canPlace
-            VoxelShape voxelShape = state.getCollisionShape(world, pos, context);
-            if (voxelShape.isEmpty() || world.isUnobstructed(player, voxelShape.move(pos.getX(), pos.getY(), pos.getZ()))) {
-                NeoForge.EVENT_BUS.post(new CanSpectatingPlaceEvent((BlockItem) (Object) this));
-                return true;
-            }
-        }
-        return world.isUnobstructed(state, pos, context);
+    @Inject(method = "canPlace", at = @At("RETURN"), cancellable = true)
+    private void canPlace(BlockPlaceContext context, BlockState state, @NotNull CallbackInfoReturnable<Boolean> cir) {
+        PlayerCanPlaceEvent event = new PlayerCanPlaceEvent(context.getPlayer(), (BlockItem) (Object) this, context, state, cir.getReturnValue());
+        NeoForge.EVENT_BUS.post(event);
+        cir.setReturnValue(event.canPlace());
     }
 }
